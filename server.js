7a95e783baa01e8048a0c625c3c8e160eefdada5
minecet <mine.cetinkaya@gmail.com>
2025-01-21 17:28:56 +0100

    main merge

--- server.js
+++ server.js
@@ -4,20 +4,5 @@ const mongoose = require('mongoose')
 const PORT = process.env.PORT || 5005
 const withDB = require('./db')
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5005
-/*const MONGO_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/restAPI'
 
-// ℹ️ Connects to the database
-const withDB = async serverListener => {
-  try {
-    const x = await mongoose.connect(MONGO_URI)
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-    if (typeof serverListener === 'function') {
-      serverListener()
-    }
-  } catch (error) {
-    console.error('Error connecting to mongo: ', err)
-  }
-}
-*/
-//module.exports = withDB
 withDB(() => {app.listen(PORT, ()=>{console.log(`server listening on http://localhost:${PORT}` )})})
